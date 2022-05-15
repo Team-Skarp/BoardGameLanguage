a8eceef6aa2c085264b15770561666747e87f880
Frederik Bode <thorbensen@gmail.com>
2022-05-16 01:14:26 +0200

    direct compilation to exe work

--- src/main/bgl/Compiler.java
+++ src/main/bgl/Compiler.java
@@ -13,12 +13,25 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
+@SuppressWarnings("rawtypes")
 public class Compiler {
 
+    // Code for StreamGobbler Taken from: https://www.baeldung.com/run-shell-command-in-java
+    private record StreamGobbler(
+            InputStream inputStream,
+            Consumer<String> consumer
+    ) implements Runnable {
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines()
+                    .forEach(consumer);
+        }
+    }
 
     private static String handleFileNaming(List<String> args) {
         String outputFileName;
@@ -63,28 +76,59 @@ public class Compiler {
         }
     }
 
-    private static void writeSTDLIB(ASTvisitor generator) {
-        try {
-            FileWriter fw = new FileWriter(
-                    "./src/main/bgl/BglFiles/Generated/bgllib.h",
-                    false
-            );
+    private static void writeSTDLIB(ASTvisitor generator) throws IOException {
+        try (FileWriter fw = new FileWriter(
+                "./src/main/bgl/BglFiles/Generated/bgllib.h",
+                false
+        )) {
             fw.write(STDLIBC.imports);
             fw.write(STDLIBC.defines);
+
             if (generator instanceof CCodeGenerator ccg) {
                 fw.write(ccg.definitions);
             }
-            fw.close();
 
             System.out.println("Compiled successfully");
         } catch (IOException ex) {
-            // Print message as exception occurred when
-            // invalid path of local machine is passed
-            System.out.print("Invalid Path");
+            throw new IOException("Could not write to file");
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    private static void writeBinary(String outputFileName) throws IOException, InterruptedException {
+        // Locate C files.
+        File stdlibFile = new File("./src/main/bgl/BglFiles/Generated/bgllib.h");
+        File userFile = new File("./src/main/bgl/BglFiles/Generated/%s".formatted(outputFileName));
+
+        if (stdlibFile.exists() && userFile.exists()) {
+            String inputFilePath = "./src/main/bgl/BglFiles/Generated/%s".formatted("test.c");
+            String outputFilePath = "./src/main/bgl/BglFiles/Generated/%s".formatted("test.exe");
+
+            // CMD command to compile with GNU compiler
+            ProcessBuilder builder = new ProcessBuilder();
+
+            // "/c" - creates a new shell, execute the provided command, and exit from the shell automatically
+            if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
+                builder.command("cmd.exe", "/c", "gcc", "./src/main/bgl/BglFiles/Generated/test.c");
+            } else {
+                builder.command("cmd", "gcc", inputFilePath, "-o", outputFilePath);
+            }
+
+//            if (System.getProperty("os.name").toLowerCase().startsWith("windows")){
+//                builder.command("cmd.exe", "/c", "dir");
+//            } else {
+//                builder.command("sh", "-c", "ls");
+//            }
+
+            Process process = builder.start();
+            StreamGobbler gobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+            Executors.newSingleThreadExecutor().submit(gobbler);
+
+            int exitCode = process.waitFor();
+            assert exitCode == 0;
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         // Setup
         CharStream input;
@@ -140,19 +184,17 @@ public class Compiler {
 
         // Pass generator to ast
         String code = (String) ast.accept(generator);
+        String outputPath = "./src/main/bgl/BglFiles/Generated/%s".formatted(outputFileName);
 
-        try {
-            FileWriter fw = new FileWriter(
-                    "./src/main/bgl/BglFiles/Generated/%s".formatted(outputFileName),
-                    false
-            );
+        try (FileWriter fw = new FileWriter(outputPath, false)) {
             fw.write(code);
-            System.out.println("Compiled successfully");
-            fw.close();
         } catch (IOException ex) {
-            // Print message as exception occurred when
-            // invalid path of local machine is passed
-            System.out.print("Invalid Path");
+            throw new IOException("Could not write to file");
+        }
+
+        // if output file format is C and -full flag is present compile with GCC
+        if (arguments.contains("-full") && arguments.contains("-c")) {
+            writeBinary(outputFileName);
         }
 
     }
